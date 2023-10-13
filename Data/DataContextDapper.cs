e69741cc6947d570777f933882e9400604b69452
Alex Stepanov <stepanov.hw@gmail.com>
2023-10-13 11:10:05 -0600

    Do Dapper Part 1 & 2

---
+++ Data/DataContextDapper.cs
@@ -0,0 +1,38 @@
+using System.Data;
+using Dapper;
+using Microsoft.Data.SqlClient;
+
+
+namespace HelloWorld.Data
+{
+    public class DataContextDapper
+    {
+        private string _connectionString = "Server=localhost;Database=DotNetCourseDatabase;Trusted_Connection=false;TrustServerCertificate=True;User Id=sa;Password=SQLConnect1;";
+
+        public IEnumerable<T> LoadData<T>(string sql)
+        {
+            IDbConnection dbConnection = new SqlConnection(_connectionString);
+            return dbConnection.Query<T>(sql);
+        }
+
+        public T LoadDataSingle<T>(string sql)
+        {
+            IDbConnection dbConnection = new SqlConnection(_connectionString);
+            return dbConnection.QuerySingle<T>(sql);
+        }
+
+        public bool ExecuteSql(string sql)
+        {
+            IDbConnection dbConnection = new SqlConnection(_connectionString);
+            return dbConnection.Execute(sql) > 0;
+        }
+
+                public int ExecuteSqlWithRowCount(string sql)
+        {
+            IDbConnection dbConnection = new SqlConnection(_connectionString);
+            return dbConnection.Execute(sql);
+        }
+
+    }
+
+}
--- Program.cs
+++ Program.cs
@@ -1,29 +1,23 @@
 ﻿using System;
+using System.Data;
+using System.Text.RegularExpressions;
+using Dapper;
+using HelloWorld.Models;
+using HelloWorld.Data;
+using Microsoft.Data.SqlClient;
 
 namespace MyApp // Note: actual namespace depends on the project name.
 {
-
-    public class Computer
-    {
-        public string Motherboard { get; set; } = "";
-
-        public int CPUCores { get; set; }
-
-        public bool HasWifi { get; set; }
-
-        public bool HasLTE { get; set; }
-
-        public DateTime ReleaseDate { get; set; }
-
-        public decimal Price { get; set; }
-
-        public string VideoCard { get; set; } = "";
-
-    }
     internal class Program
     {
         static void Main(string[] args)
         {
+
+            DataContextDapper dapper = new DataContextDapper();
+
+            DateTime rightNow = dapper.LoadDataSingle<DateTime>("SELECT GETDATE()");
+
+            // Console.WriteLine(rightNow);
 
             Computer myComputer = new Computer()
             {
@@ -35,12 +29,55 @@ namespace MyApp // Note: actual namespace depends on the project name.
                 VideoCard = "RTX 2060"
             };
 
-            Console.WriteLine(myComputer.Motherboard);
-            Console.WriteLine(myComputer.HasWifi);
-            Console.WriteLine(myComputer.HasLTE);
-            Console.WriteLine(myComputer.ReleaseDate);
-            Console.WriteLine(myComputer.Price);
-            Console.WriteLine(myComputer.VideoCard);
+            string sql = @"INSERT INTO TutorialAppSchema.Computer (
+                Motherboard,
+                HasWifi,
+                HasLTE,
+                ReleaseDate,
+                Price,
+                VideoCard
+            ) VALUES ('" + myComputer.Motherboard
+             + "','" + myComputer.HasWifi
+             + "','" + myComputer.HasLTE
+             + "','" + myComputer.ReleaseDate
+             + "','" + myComputer.Price
+             + "','" + myComputer.VideoCard
+             + "')";
+            // Console.WriteLine(sql);
+            //  int result = dapper.ExecuteSqlWithRowCount(sql);
+            bool result = dapper.ExecuteSql(sql);
+
+
+            // Console.WriteLine(result);
+
+            string sqlSelect = @"SELECT
+                Computer.Motherboard,
+                Computer.HasWifi,
+                Computer.HasLTE,
+                Computer.ReleaseDate,
+                Computer.Price,
+                Computer.VideoCard
+            FROM TutorialAppSchema.Computer";
+
+            IEnumerable<Computer> computers = dapper.LoadData<Computer>(sqlSelect);
+
+            Console.WriteLine("'Motherboard','HasWifi','HasLTE','ReleaseDate','Price','VideoCard'");
+            foreach(Computer singleComputer in computers) {
+                Console.WriteLine("'" + myComputer.Motherboard
+             + "','" + myComputer.HasWifi
+             + "','" + myComputer.HasLTE
+             + "','" + myComputer.ReleaseDate
+             + "','" + myComputer.Price
+             + "','" + myComputer.VideoCard
+             + "'");
+            }
+
+            // Console.WriteLine(myComputer.Motherboard);
+            // Console.WriteLine(myComputer.HasWifi);
+            // Console.WriteLine(myComputer.HasLTE);
+            // Console.WriteLine(myComputer.ReleaseDate);
+            // Console.WriteLine(myComputer.Price);
+            // Console.WriteLine(myComputer.VideoCard);
 
         }
     }

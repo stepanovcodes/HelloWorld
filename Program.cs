8eca3f3347bdf96bbcc0701a76f0bf1784f38e08
Alex Stepanov <stepanov.hw@gmail.com>
2023-10-24 11:27:47 -0600

    Do File Read and Write

--- Program.cs
+++ Program.cs
@@ -6,6 +6,7 @@ using HelloWorld.Models;
 using HelloWorld.Data;
 using Microsoft.Data.SqlClient;
 using Microsoft.Extensions.Configuration;
+using Microsoft.Identity.Client;
 
 namespace MyApp // Note: actual namespace depends on the project name.
 {
@@ -13,15 +14,6 @@ namespace MyApp // Note: actual namespace depends on the project name.
     {
         static void Main(string[] args)
         {
-
-            IConfiguration config = new ConfigurationBuilder()
-               .AddJsonFile("appsettings.json")
-               .Build();
-
-            DataContextDapper dapper = new DataContextDapper(config);
-            DataContextEF entityFramework = new DataContextEF(config);
-
-            DateTime rightNow = dapper.LoadDataSingle<DateTime>("SELECT GETDATE()");
 
             // Console.WriteLine(rightNow);
 
@@ -34,9 +26,6 @@ namespace MyApp // Note: actual namespace depends on the project name.
                 Price = 943.87m,
                 VideoCard = "RTX 2060"
             };
-
-            entityFramework.Add(myComputer);
-            entityFramework.SaveChanges();
 
             string sql = @"INSERT INTO TutorialAppSchema.Computer (
                 Motherboard,
@@ -51,64 +40,19 @@ namespace MyApp // Note: actual namespace depends on the project name.
              + "','" + myComputer.ReleaseDate
              + "','" + myComputer.Price
              + "','" + myComputer.VideoCard
-             + "')";
-            // Console.WriteLine(sql);
-            //  int result = dapper.ExecuteSqlWithRowCount(sql);
-            bool result = dapper.ExecuteSql(sql);
+             + "')\n";
 
+            // File.WriteAllText("log.txt", sql);
 
-            // Console.WriteLine(result);
+            using StreamWriter openFile = new("log.txt", append: true);
 
-            string sqlSelect = @"SELECT
-                Computer.ComputerId,
-                Computer.Motherboard,
-                Computer.HasWifi,
-                Computer.HasLTE,
-                Computer.ReleaseDate,
-                Computer.Price,
-                Computer.VideoCard
-            FROM TutorialAppSchema.Computer";
+            openFile.WriteLine(sql);
 
-            IEnumerable<Computer> computers = dapper.LoadData<Computer>(sqlSelect);
+            openFile.Close();
 
-            Console.WriteLine("'ComputerId','Motherboard','HasWifi','HasLTE','ReleaseDate','Price','VideoCard'");
-            foreach (Computer singleComputer in computers)
-            {
-                Console.WriteLine("'" + singleComputer.ComputerId
-             + "','" + singleComputer.Motherboard
-             + "','" + singleComputer.HasWifi
-             + "','" + singleComputer.HasLTE
-             + "','" + singleComputer.ReleaseDate
-             + "','" + singleComputer.Price
-             + "','" + singleComputer.VideoCard
-             + "'");
-            }
+            string fileText = File.ReadAllText("log.txt");
 
-            IEnumerable<Computer>? computersEF = entityFramework.Computer?.ToList<Computer>();
-            if (computersEF != null)
-            {
-
-                Console.WriteLine("'ComputerId','Motherboard','HasWifi','HasLTE','ReleaseDate','Price','VideoCard'");
-                foreach (Computer singleComputer in computersEF)
-                {
-                    Console.WriteLine("'" + singleComputer.ComputerId
-             + "','" + singleComputer.Motherboard
-             + "','" + singleComputer.HasWifi
-             + "','" + singleComputer.HasLTE
-             + "','" + singleComputer.ReleaseDate
-             + "','" + singleComputer.Price
-             + "','" + singleComputer.VideoCard
-             + "'");
-                }
-
-            }
-
-            // Console.WriteLine(myComputer.Motherboard);
-            // Console.WriteLine(myComputer.HasWifi);
-            // Console.WriteLine(myComputer.HasLTE);
-            // Console.WriteLine(myComputer.ReleaseDate);
-            // Console.WriteLine(myComputer.Price);
-            // Console.WriteLine(myComputer.VideoCard);
+            Console.WriteLine(fileText);
 
         }
     }
